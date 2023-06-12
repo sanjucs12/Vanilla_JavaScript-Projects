e6c96e8f160eb4b13b111c25f91cbc4844db632b
sanjucs12 <sanjucs12@gmail.com>
2023-06-12 20:07:40 +0530

    sidebar

---
+++ 05. Sidebar/app.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector('.close-btn')
+const sideBar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", () => {
+  sideBar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+    sideBar.classList.remove("show-sidebar");
+  });
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. color flipper --->  [startling-sunburst-b8ab90.netlify.app](https://startling-sunburst-b8ab90.netlify.app/)
 2. Counter---------->  https://cerulean-sunshine-f56f84.netlify.app/
 3. Reviews --------->  https://melodious-biscuit-7534c0.netlify.app/
+4. Navbar ---------->
+5. Sidebar---------->
